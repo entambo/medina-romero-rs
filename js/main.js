6484251361d54e746f1a99d46a73146289453cef
Esteban Tamborini <tamboriniesteban44@gmail.com>
2023-01-19 09:57:48 -0300

    agregados

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    /* Cambio dehheader */
     $(window).on('scroll', function() { 
         let posicion = $('#pr').offset();
         if ($(window).scrollTop() >= posicion.top) {
@@ -6,5 +7,22 @@ $(document).ready(function() {
         } else{
             $('header').addClass('transparente');
         }
+    })
+    /* Acndicionamiento del menu lateral */
+    $(document).ready(function() {
+        $('#burger').click(function() {
+            $('#burger').toggleClass('abierto');
+            $('.menu').toggleClass('abierto');
+        })
+    })
+    //Smoth scroll//
+    $('a').on('click', function() {
+        if (this.hash !== '') {
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 750)
+        }
+
     })
 })
